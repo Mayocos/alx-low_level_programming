ee70bfde7919180aad429616d2f242a28ed9bc0e
Mayocos <mayowaoluwaseun0@gmail.com>
2023-02-22 07:01:14 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Test functions for positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(1);
+
+return (0);
+}
+
