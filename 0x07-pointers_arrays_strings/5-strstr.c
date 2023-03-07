6bd4de094c14cfa496abd0c863bcc84ac6fae6ea
Mayocos <mayowaoluwaseun0@gmail.com>
2023-03-06 18:36:57 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,11 +8,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 for (; *haystack != '\0'; haystack++)
-}
+{
 while (*l == *p && *p != '\0')
 {
 l++;
 p++;
+}
 if (*p == '\0')
 return (haystack);
 }
